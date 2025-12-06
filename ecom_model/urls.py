6dd82664b2f746892cb31b012bef52bed50b5d5c
Mustafa Prodhan <mufassagit@gmail.com>
2025-12-06 00:34:56 -0500

    settup the utls for store and settings

--- ecom_model/urls.py
+++ ecom_model/urls.py
@@ -15,8 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    # Admin url
     path('admin/', admin.site.urls),
+    # Store app
+    path('', include('store.urls')),
+    # Cart app
+    #path('cart/', include('cart.urls')),
+    # Account app
+    #path('account/', include('account.urls')),
+    # Payment app
+    #path('payment/', include('payment.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
