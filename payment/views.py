a00f953aca5e36ef63f18375e0b1a050d10ea8c7
Mustafa Prodhan <mufassagit@gmail.com>
2025-12-23 10:14:48 -0500

    all payments succeed and an email gets sent

--- payment/views.py
+++ payment/views.py
@@ -6,8 +6,11 @@ from . models import ShippingAddress, Order, OrderItem
 
 from cart.cart import Cart
 
-
 from django.http import JsonResponse
+
+from django.core.mail import send_mail
+
+from django.conf import settings
 
 
 def checkout(request):
@@ -58,89 +61,78 @@ def complete_order(request):
         state = request.POST.get('state')
         zipcode = request.POST.get('zipcode')
 
-
         # All-in-one shipping address
-
         shipping_address = (address1 + "\n" + address2 + "\n" +
-        
-        city + "\n" + state + "\n" + zipcode
-        
-        )
+        city + "\n" + state + "\n" + zipcode)
 
         # Shopping cart information 
-
         cart = Cart(request)
 
-
         # Get the total price of items
-
         total_cost = cart.get_total()
 
-
-        '''
-
-            Order variations
-
-            1) Create order -> Account users WITH + WITHOUT shipping information
-        
-
-            2) Create order -> Guest users without an account
-        
-
-        '''
+        # Initialize product list
+        product_list = []
 
         # 1) Create order -> Account users WITH + WITHOUT shipping information
-
         if request.user.is_authenticated:
 
-            order = Order.objects.create(full_name=name, email=email, shipping_address=shipping_address,
-            
-            amount_paid=total_cost, user=request.user)
-
+            order = Order.objects.create(
+                full_name=name, 
+                email=email, 
+                shipping_address=shipping_address,
+                amount_paid=total_cost, 
+                user=request.user
+            )
 
             order_id = order.pk
 
             for item in cart:
-
-                OrderItem.objects.create(order_id=order_id, product=item['product'], quantity=item['qty'],
-                
-                price=item['price'], user=request.user)
-
+                OrderItem.objects.create(
+                    order_id=order_id, 
+                    product=item['product'], 
+                    quantity=item['qty'],
+                    price=item['price'], 
+                    user=request.user
+                )
+                product_list.append(item['product'])
 
         #  2) Create order -> Guest users without an account
-
         else:
 
-            order = Order.objects.create(full_name=name, email=email, shipping_address=shipping_address,
-            
-            amount_paid=total_cost)
-
+            order = Order.objects.create(
+                full_name=name, 
+                email=email, 
+                shipping_address=shipping_address,
+                amount_paid=total_cost
+            )
 
             order_id = order.pk
 
-
             for item in cart:
+                OrderItem.objects.create(
+                    order_id=order_id, 
+                    product=item['product'], 
+                    quantity=item['qty'],
+                    price=item['price']
+                )
+                product_list.append(item['product'])
 
-                OrderItem.objects.create(order_id=order_id, product=item['product'], quantity=item['qty'],
-                
-                price=item['price'])
-
-
+        # Send email once for both user types
+        send_mail(
+            'Order received', 
+            'Hi! ' +  '\n\n' + 'Thank you for placing your order' + '\n\n' +
+            'Please see your order below: ' + '\n\n' +  str(product_list)  + '\n\n' + 
+            'Total paid: $' + str(total_cost), 
+            settings.EMAIL_HOST_USER, 
+            [email], 
+            fail_silently=False
+        )
 
         order_success = True
-
-        response = JsonResponse({'success':order_success})
+        response = JsonResponse({'success': order_success})
 
         return response
-
-
-
-
-
-
-    
-
-
 
 
 def payment_success(request):
@@ -158,11 +150,6 @@ def payment_success(request):
 
 
     return render(request, 'payment/payment-success.html')
-
-
-
-
-
 
 
 def payment_failed(request):
