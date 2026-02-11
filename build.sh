a87e93d54a6458a101476ca34e8f556b15a674f1
Mustafa Prodhan <mufassagit@gmail.com>
2026-02-11 02:54:52 -0500

    modified the build file

--- build.sh
+++ build.sh
@@ -5,14 +5,3 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-```
-
-Then make it executable locally with `chmod a+x build.sh` and push it to your repo.
-
-**B. Update `settings.py`** in your `ecom_model/` folder. You'll need to add a few things:
-
-1. Install the `dj-database-url` and `gunicorn` packages — add them to your `requirements.txt`:
-```
-gunicorn
-dj-database-url
-psycopg2-binary
--- ecom_model/settings.py
+++ ecom_model/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 #STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'static/media'
